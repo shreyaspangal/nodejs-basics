2e229fc4634733b78e66d73d7e1cf878fcb6fad0
Shreyas Pangal <shreyaspangal03@gmail.com>
2022-04-09 21:37:41 +0530

    feat: handle delete api request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // File: app.js
 
 const http = require("http");
+const urlModule = require("url");
 
 const port = 8081;
 
@@ -9,8 +10,10 @@ let todoList = ["Complete Node Byte", "Play Cricket"];
 http
   .createServer((request, response) => {
     const { method, url } = request;
+    let q = urlModule.parse(`${url}?name=playcricket`, true);
+    let qData = q.query; //<br/><br/> url: ${url} &&  q: ${qData.name}
 
-    if (url == "/todos") {
+    if (url === "/todos") {
       if (method == "GET") {
         response.writeHead(200, { "Content-Type": "text/html" });
         //Content
@@ -30,9 +33,30 @@ http
           .on("end", () => {
             body = JSON.parse(body);
             let newTodo = body.name;
-            todoList.push(body.name);
-
+            todoList.push(newTodo);
+            //Status Code
             response.writeHead(201);
+          });
+      } else if (method === "DELETE") {
+        let body = "";
+        request
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            body += chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let deleteTodo = body.name;
+
+            for (let i = 0; i < todoList.length; i++) {
+              if (todoList[i] === deleteTodo) {
+                todoList.splice(i, 1);
+              }
+            }
+            //Status Code
+            response.writeHead(204);
           });
       } else {
         response.writeHead(501);
